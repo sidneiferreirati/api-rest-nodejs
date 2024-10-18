daba16dabb9dd56c2e68d6c54970f8eedf6a443f
sidnei <sidneiferreirati@gmail.com>
2024-10-18 09:58:31 -0300

    route params

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from "express";
 
-const PORT = 3000;
+const PORT = 3333;
 
 const app = express();
+
+app.post("/products/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`produto ${id} criado com sucesso!`);
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
